c9430595ed13ddafd03cdfa5e377bd34f668003b
Tom <fougeretom@gmail.com>
2022-01-22 16:22:39 +0100

    In utils, create function to get the project folder path

--- utils/text_operations.py
+++ utils/text_operations.py
@@ -1,3 +1,6 @@
+import os
+
+
 def find_substring_with_dict(text, dictionary):
 
     value = None
@@ -13,3 +16,7 @@ def remove_substring_with_list(text, list_substrings):
     for substring in list_substrings:
         text = text.replace(substring, '')
     return text
+
+
+def get_project_root() -> str:
+    return os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
