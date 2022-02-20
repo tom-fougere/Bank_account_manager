26fce08e34fcd223b90875b81b90347ddf45900e
Tom <fougeretom@gmail.com>
2022-02-20 17:00:12 +0100

    Modify functions use to update canvas

--- apps/canvas/canvas.py
+++ apps/canvas/canvas.py
@@ -2,15 +2,120 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash import Input, Output, State, callback_context
+import dash_daq as daq
 import pandas as pd
 import json
-from apps.canvas.canvas_transaction_details import create_canvas_content_with_transaction_details
+from datetime import date
+from apps.canvas.canvas_transaction_details import get_transaction_values
+from source.transactions.transaction_operations import get_sub_categories, get_occasion, get_categories
+from source.definitions import DB_CONN_ACCOUNT, ACCOUNT_ID
 
 from app import app
 
+
+transaction_details_layout = html.Div([
+    html.Div([
+        'Compte bancaire:',
+        dcc.Input(
+            id='canvas_account_id',
+            style={'width': '100%'},
+            disabled=True),
+    ],
+        style={'margin-top': 10}),
+    html.Div([
+        html.Div([
+            html.Div('Date Transaction:'),
+            dcc.DatePickerSingle(
+                id='canvas_date_transaction',
+                disabled=True),
+        ],
+            style={'width': '50%'}),
+        html.Div([
+            html.Div('Date à la banque:'),
+            dcc.DatePickerSingle(
+                id='canvas_date',
+                disabled=True),
+        ],
+            style={'width': '50%'}
+        )],
+        style={"display": 'flex',
+               'margin-top': 10}),
+    html.Div([
+        'Libelé:',
+        dcc.Textarea(
+            id='canvas_description',
+            style={'width': '100%'},
+            disabled=True),
+    ],
+        style={'margin-top': 10}),
+    html.Div([
+        'Montant (€):',
+        dcc.Input(
+            id='canvas_amount',
+            style={'width': '100%'},
+            type='number',
+            disabled=True),
+    ],
+        style={'margin-top': 10}),
+    html.Div([
+        'Catégorie:',
+        dcc.Dropdown(
+            id='canvas_category',
+            options=get_categories(db_connection=DB_CONN_ACCOUNT,
+                                   account_id=ACCOUNT_ID),
+            # value=df.category,
+            multi=False,
+            style={'width': '100%'},
+            disabled=True),
+        dcc.Dropdown(
+            id='canvas_sub_category',
+            multi=False,
+            style={'width': '100%'},
+            disabled=True),
+    ],
+        style={'margin-top': 10}),
+    html.Div([
+        'Occasion:',
+        dcc.Dropdown(
+            id='canvas_occasion',
+            options=get_occasion(db_connection=DB_CONN_ACCOUNT,
+                                 account_id=ACCOUNT_ID),
+            multi=False,
+            style={'width': '100%'},
+            disabled=True),
+    ],
+        style={'margin-top': 10}),
+    html.Div([
+        html.Div('Type:'),
+        dcc.Dropdown(
+            id='canvas_type',
+            options=[],
+            multi=False,
+            style={'width': '100%'},
+            disabled=True),
+    ],
+        style={'margin-top': 10}),
+    html.Div([
+        'Note:',
+        dcc.Textarea(
+            id='canvas_note',
+            style={'width': '100%'},
+            disabled=True),
+    ],
+        style={'margin-top': 10}),
+    html.Div([
+        'Pointage:',
+        daq.BooleanSwitch(
+            id='canvas_check',
+            disabled=True),
+    ],
+        style={'margin-top': 10}),
+])
+
+
 canvas = dbc.Offcanvas(
     html.Div([
-        html.Div(id='canvas_transaction'),
+        transaction_details_layout,
         html.Button(
             'Enregistrer',
             id='save_trans_details',
@@ -28,34 +133,101 @@ canvas = dbc.Offcanvas(
 
 
 @app.callback(
-    [Output("canvas_transaction", "children"),
-     Output("save_trans_details", "disabled"),
-     Output("off_canvas", "is_open")],
+    [Output("canvas_account_id", "value"),
+     Output("canvas_date_transaction", "date"),
+     Output("canvas_date", "date"),
+     Output("canvas_description", "value"),
+     Output("canvas_amount", "value"),
+     Output("canvas_category", "value"),
+     Output("canvas_sub_category", "options"),
+     Output("canvas_sub_category", "value"),
+     Output("canvas_occasion", "value"),
+     Output("canvas_type", "value"),
+     Output("canvas_note", "value"),
+     Output("canvas_check", "value")],
     [Input('store_transaction_disabled', 'data'),
-     Input('store_transaction_enabled', 'data')],
-    State("off_canvas", "is_open"))
-def open_canvas(jsonified_data_disabled_trans, jsonified_data_enabled_trans, canvas_is_open):
-
+     Input('store_transaction_enabled', 'data')])
+def update_transaction_values(jsonified_data_disabled_trans, jsonified_data_enabled_trans):
     ctx = callback_context
     triggered_input = ctx.triggered[0]['prop_id'].split('.')[0]
 
     if (triggered_input == 'store_transaction_disabled') and (jsonified_data_disabled_trans is not None):
         parsed = json.loads(jsonified_data_disabled_trans)
         df = pd.Series(parsed)
-        component = create_canvas_content_with_transaction_details(df, disabled=True)
-        save_button_disable = True
-        canvas_new_update = not canvas_is_open
+        transaction_values = get_transaction_values(df)
     elif (triggered_input == 'store_transaction_enabled') and (jsonified_data_enabled_trans is not None):
         parsed = json.loads(jsonified_data_enabled_trans)
         df = pd.Series(parsed)
-        component = create_canvas_content_with_transaction_details(df, disabled=False)
-        save_button_disable = False
-        canvas_new_update = not canvas_is_open
+        transaction_values = get_transaction_values(df)
     else:
-        component = html.Div()
-        save_button_disable = True
-        canvas_new_update = canvas_is_open
+        transaction_values = (None,) * 12
 
-    clear_data = True
+    return transaction_values
 
-    return component, save_button_disable, canvas_new_update
+
+@app.callback(
+    [Output("save_trans_details", "disabled"),
+     Output("canvas_date", "disabled"),
+     Output("canvas_description", "disabled"),
+     Output("canvas_amount", "disabled"),
+     Output("canvas_category", "disabled"),
+     Output("canvas_sub_category", "disabled"),
+     Output("canvas_occasion", "disabled"),
+     Output("canvas_type", "disabled"),
+     Output("canvas_note", "disabled"),
+     Output("canvas_check", "disabled")],
+    [Input('store_transaction_disabled', 'data'),
+     Input('store_transaction_enabled', 'data')])
+def update_transaction_disable(jsonified_data_disabled_trans, jsonified_data_enabled_trans):
+    ctx = callback_context
+    triggered_input = ctx.triggered[0]['prop_id'].split('.')[0]
+
+    nb_outputs = 9
+    disabled = True
+    if (triggered_input == 'store_transaction_disabled') and (jsonified_data_disabled_trans is not None):
+        msg = (disabled,) * nb_outputs
+        save_button = disabled
+    elif (triggered_input == 'store_transaction_enabled') and (jsonified_data_enabled_trans is not None):
+        msg = (not disabled,) * nb_outputs
+        save_button = not disabled
+    else:
+        msg = (disabled,) * nb_outputs
+        save_button = disabled
+    return (save_button,) + msg
+
+
+@app.callback(
+    Output("off_canvas", "is_open"),
+    [Input('store_transaction_disabled', 'data'),
+     Input('store_transaction_enabled', 'data')],
+    State("off_canvas", "is_open"))
+def update_transaction_disable(data1, data2, canvas_is_open):
+    ctx = callback_context
+    triggered_input = ctx.triggered[0]['prop_id'].split('.')[0]
+
+    canvas_new_update = canvas_is_open
+    if (triggered_input == 'store_transaction_disabled') and (data1 is not None):
+        canvas_new_update = not canvas_is_open
+    elif (triggered_input == 'store_transaction_enabled') and (data2 is not None):
+        canvas_new_update = not canvas_is_open
+
+    return canvas_new_update
+
+
+# @app.callback(
+#     Output('canvas_sub_category', 'options'),
+#     Input('canvas_category', 'value')
+# )
+# def update_sub_category(value):
+#     # Default value
+#     dropdown_sub_category = [{}]
+#     changed_id = [p['prop_id'] for p in callback_context.triggered][0]
+#
+#     if value is not None and len(value) > 0:
+#         dropdown_sub_category = get_sub_categories(
+#             db_connection=DB_CONN_ACCOUNT,
+#             account_id=ACCOUNT_ID,
+#             categories=[value],
+#             add_suffix_cat=False),
+#
+#     return dropdown_sub_category
--- apps/canvas/canvas_transaction_details.py
+++ apps/canvas/canvas_transaction_details.py
@@ -5,45 +5,51 @@ from datetime import date
 
 from utils.time_operations import str_to_datetime
 from source.definitions import DB_CONN_ACCOUNT
-from source.transactions.transaction_operations import get_categories, get_occasion
+from source.transactions.transaction_operations import get_categories, get_sub_categories, get_occasion
 
 
 def create_canvas_content_with_transaction_details(df, disabled=True):
-
     date_transaction = str_to_datetime(df.date_transaction_str, date_format='%d/%m/%Y')
     date_bank = str_to_datetime(df.date_str, date_format='%d/%m/%Y')
+    if df.category is not None:
+        sub_categories = get_sub_categories(
+                        db_connection=DB_CONN_ACCOUNT,
+                        account_id=df.account_id,
+                        categories=[df.category],
+                        add_suffix_cat=False)
+    else:
+        sub_categories = []
 
     layout = html.Div([
         html.Div([
             'Compte bancaire:',
             dcc.Input(
-                id='sidebar_account_id',
+                id='canvas_account_id',
                 value=df.account_id,
                 style={'width': '100%'},
-                type='number',
                 disabled=True),
-            ],
+        ],
             style={'margin-top': 10}),
         html.Div([
             html.Div([
                 html.Div('Date Transaction:'),
                 dcc.DatePickerSingle(
-                    id='sidebar_date_transaction',
+                    id='canvas_date_transaction',
                     date=date(date_transaction.year,
                               date_transaction.month,
                               date_transaction.day),
                     disabled=disabled),
-                ],
+            ],
                 style={'width': '50%'}),
             html.Div([
                 html.Div('Date à la banque:'),
                 dcc.DatePickerSingle(
-                    id='sidebar_date',
+                    id='canvas_date',
                     date=date(date_bank.year,
                               date_bank.month,
                               date_bank.day),
                     disabled=True),
-                ],
+            ],
                 style={'width': '50%'}
             )],
             style={"display": 'flex',
@@ -51,83 +57,114 @@ def create_canvas_content_with_transaction_details(df, disabled=True):
         html.Div([
             'Libelé:',
             dcc.Textarea(
-                id='sidebar_description',
+                id='canvas_description',
                 value=df.description,
                 style={'width': '100%'},
                 disabled=disabled),
-            ],
+        ],
             style={'margin-top': 10}),
-         html.Div([
+        html.Div([
             'Montant (€):',
             dcc.Input(
-                id='sidebar_amount',
+                id='canvas_amount',
                 value=df.amount,
                 style={'width': '100%'},
                 type='number',
                 disabled=disabled),
-             ],
+        ],
             style={'margin-top': 10}),
         html.Div([
             'Catégorie:',
             dcc.Dropdown(
-                id='sidebar_category',
+                id='canvas_category',
                 options=get_categories(db_connection=DB_CONN_ACCOUNT,
                                        account_id=df.account_id),
-                value=df.category,
+                # value=df.category,
                 multi=False,
                 style={'width': '100%'},
                 disabled=disabled),
             dcc.Dropdown(
-                id='sidebar_sub_category',
-                options=get_categories(db_connection=DB_CONN_ACCOUNT,
-                                       account_id=df.account_id),
-                value=df.sub_category,
+                id='canvas_sub_category',
+                options=sub_categories,
+                # value=df.sub_category,
                 multi=False,
                 style={'width': '100%'},
                 disabled=disabled),
+            # sub_cat_layout,
             ],
             style={'margin-top': 10}),
         html.Div([
             'Occasion:',
             dcc.Dropdown(
-                id='sidebar_occasion',
+                id='canvas_occasion',
                 options=get_occasion(db_connection=DB_CONN_ACCOUNT,
                                      account_id=df.account_id),
                 value=df.occasion,
                 multi=False,
                 style={'width': '100%'},
                 disabled=disabled),
-            ],
+        ],
             style={'margin-top': 10}),
         html.Div([
             html.Div('Type:'),
             dcc.Dropdown(
-                id='sidebar_type',
+                id='canvas_type',
                 options=[],
                 value=df.type_transaction,
                 multi=False,
                 style={'width': '100%'},
                 disabled=disabled),
-            ],
+        ],
             style={'margin-top': 10}),
         html.Div([
             'Note:',
             dcc.Textarea(
-                id='sidebar_note',
+                id='canvas_note',
                 value=df.note,
                 style={'width': '100%'},
                 disabled=disabled),
-            ],
+        ],
             style={'margin-top': 10}),
         html.Div([
             'Pointage:',
             daq.BooleanSwitch(
-                id='sidebar_check',
+                id='canvas_check',
                 on=df.check,
                 disabled=disabled),
-            ],
+        ],
             style={'margin-top': 10}),
     ])
 
     return layout
 
+
+def get_transaction_values(df):
+    datetime_transaction = str_to_datetime(df.date_transaction_str, date_format='%d/%m/%Y')
+    datetime_bank = str_to_datetime(df.date_str, date_format='%d/%m/%Y')
+
+    account_id = df.account_id
+    date_transaction = date(datetime_transaction.year,
+                            datetime_transaction.month,
+                            datetime_transaction.day)
+    date_bank = date(datetime_bank.year,
+                     datetime_bank.month,
+                     datetime_bank.day)
+    description = df.description
+    amount = df.amount
+    category = df.category
+    if df.category is not None:
+        sub_categories = get_sub_categories(
+                        db_connection=DB_CONN_ACCOUNT,
+                        account_id=df.account_id,
+                        categories=[df.category],
+                        add_suffix_cat=False)
+    else:
+        sub_categories = []
+    sub_category = df.sub_category
+    occasion = df.occasion
+    transaction_type = df.type_transaction
+    note = df.note
+    check = df.check
+
+    return (account_id, date_transaction, date_bank, description, amount, category, sub_categories, sub_category,
+            occasion, transaction_type, note, check)
