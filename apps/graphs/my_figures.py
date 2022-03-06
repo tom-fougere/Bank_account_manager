d9a2b0dadde817723a695d1cdd9ab6d8c0cdcf66
Tom <fougeretom@gmail.com>
2022-03-06 19:18:38 +0100

    Make subplot to display balance

--- apps/graphs/my_figures.py
+++ apps/graphs/my_figures.py
@@ -1,7 +1,7 @@
-import pandas as pd
+import numpy as np
 import datetime
-import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 from source.definitions import DB_CONN_TRANSACTION
 from source.db_connection.db_access import MongoDBConnection
@@ -22,23 +22,42 @@ def fig_expenses_vs_gain():
     df = transExgest.exgest()
 
     # Transform df
-    df['Total_negative'] = - df['Total_negative']
-    df = expand_columns_of_dataframe(df, column='_id')
-    df['date'] = [datetime.datetime(int(row['Année']), int(row['Mois']), 1) for _, row in df.iterrows()]
+    df = expand_columns_of_dataframe(df, column='_id')  # Expand ID to get date
+    df['date'] = [datetime.datetime(int(row['Année']), int(row['Mois']), 1) for _, row in df.iterrows()]  # convert in datetime
+    df['Total_negative'] = - df['Total_negative']  # Negative becomes Positive
+    df["Color"] = np.where(df["Balance"] < 0, '#EF553B', '#636EFA')  # Change color following sign
     df.sort_values(by='date', inplace=True)
 
-    figure = go.Figure()
+    # Figures
+    figure = make_subplots(rows=2, cols=1)
     figure.add_trace(go.Bar(
         x=df['date'],
         y=df['Total_positive'],
         name='Gains'
-    ))
+        ),
+        row=1, col=1)
     figure.add_trace(go.Bar(
         x=df['date'],
         y=df['Total_negative'],
         name='Dépenses'
-    ))
+        ),
+        row=1, col=1)
+    figure.add_trace(go.Bar(
+        x=df['date'],
+        y=df['Balance'],
+        name='Balance',
+        marker_color=df['Color']
+        ),
+        row=2, col=1)
+    figure.add_trace(go.Scatter(
+        x=df['date'],
+        y=np.zeros(df['date'].shape),
+        mode='lines',
+        line=dict(dash='dash', color='black')
+        ),
+        row=2, col=1)
     figure.update_layout(xaxis=dict(tickformat="%b \n%Y"))
+    figure.update_layout(xaxis2=dict(tickformat="%b \n%Y"))
 
     return figure
 
