3598269a2c3cbfdfb3a527259617aede11c65813
Tom <fougeretom@gmail.com>
2022-03-20 19:38:14 +0100

    Add boolean switch to search criteria

--- apps/search_data/search_data.py
+++ apps/search_data/search_data.py
@@ -1,23 +1,54 @@
 import dash_core_components as dcc
+import dash_bootstrap_components as dbc
 import dash_html_components as html
 import dash_table as dt
+import dash_daq as daq
 from dash import Input, Output, State, callback_context
 from app import app
 
 from datetime import date
 from apps.search_data.operations import search_transactions, create_datatable
 from source.transactions.transaction_operations import get_categories, get_sub_categories, get_occasion
-from source.definitions import DB_CONN_ACCOUNT, DB_CONN_TRANSACTION, ACCOUNT_ID
+from source.definitions import DB_CONN_ACCOUNT, DB_CONN_TRANSACTION, ACCOUNT_ID,\
+    CATEGORIES, OCCASIONS, TYPE_TRANSACTIONS
 
 layout = html.Div([
     html.Div([
-        html.Div("Date de transaction:"),
-        dcc.DatePickerRange(
-            id='search_date',
-            end_date=date.today())
-        ]),
+        html.Div([
+            html.Div("Date de transaction:"),
+            dcc.DatePickerRange(
+                id='search_date',
+                end_date=date.today())
+            ], style={'width': '50%'}),
+        html.Div([
+            html.Div("Pointage:"),
+            dbc.RadioItems(
+                id="radios",
+                className="btn-group",
+                inputClassName="btn-check",  # Remove items circles
+                labelClassName="btn btn-outline-primary",
+                labelCheckedClassName="active",
+                options=[
+                    {"label": "Non", "value": 1},
+                    {"label": "-", "value": 2},
+                    {"label": "Oui", "value": 3},
+                ],
+                value=2,
+                style={"height": 30}
+            ),
+        ], className="radio-group"),
+    ], style={'display': 'flex',
+              'margin-top': 10}),
     html.Div([
-        'Montant (€):',
+        html.Div('Montant (€):'),
+        daq.BooleanSwitch(
+            id='bool_amount',
+            on=False,
+            style={"margin-left": 10}),
+        ],
+        style={'margin-top': 10,
+               "display": 'flex'}),
+    html.Div([
         html.Div([
             dcc.Input(
                 id='search_amount_min',
@@ -36,12 +67,21 @@ layout = html.Div([
         ],
         style={'margin-top': 10}),
     html.Div([
-        "Catégorie:",
+        html.Div('Catégorie:'),
+        daq.BooleanSwitch(
+            id='bool_category',
+            on=False,
+            style={"margin-left": 10}),
+    ],
+        style={'margin-top': 10,
+               "display": 'flex'}),
+    html.Div([
         html.Div([
             dcc.Dropdown(
                 id='search_category',
-                options=get_categories(db_connection=DB_CONN_ACCOUNT,
-                                       account_id=ACCOUNT_ID),
+                options=[{'label': cat, 'value': cat} for cat in CATEGORIES.keys()],
+                # options=get_categories(db_connection=DB_CONN_ACCOUNT,
+                #                        account_id=ACCOUNT_ID),
                 value=[],
                 multi=True,
                 style={'width': '100%'}
@@ -53,22 +93,39 @@ layout = html.Div([
         style={'margin-top': 10}),
     html.Div([
         html.Div([
-            'Occasion:',
+            html.Div([
+                html.Div('Occasion:'),
+                daq.BooleanSwitch(
+                    id='bool_occasion',
+                    on=False,
+                    style={"margin-left": 10}),
+            ],
+                style={"display": 'flex'}),
             dcc.Dropdown(
                 id='search_occasion',
-                options=get_occasion(db_connection=DB_CONN_ACCOUNT,
-                                     account_id=ACCOUNT_ID),
+                options=[{'label': occ, 'value': occ} for occ in OCCASIONS],
+                # options=get_occasion(db_connection=DB_CONN_ACCOUNT,
+                #                      account_id=ACCOUNT_ID),
                 value=[],
                 multi=True,
+                style={'margin-top': 10}
             )],
             style={'width': '50%'}),
         html.Div([
-            "Type transaction:",
+            html.Div([
+                html.Div('Type transaction:'),
+                daq.BooleanSwitch(
+                    id='bool_type',
+                    on=False,
+                    style={"margin-left": 10}),
+            ],
+                style={"display": 'flex'}),
             dcc.Dropdown(
                 id='search_type',
-                options=[],
+                options=[{'label': tt, 'value': tt} for tt in TYPE_TRANSACTIONS],
                 value=[],
                 multi=True,
+                style={'margin-top': 10}
             )],
             style={'width': '50%'})
         ],
@@ -76,21 +133,37 @@ layout = html.Div([
                'margin-top': 10}),
     html.Div([
         html.Div([
-            html.Div("Libelé:"),
+            html.Div([
+                html.Div('Libelé:'),
+                daq.BooleanSwitch(
+                    id='bool_description',
+                    on=False,
+                    style={"margin-left": 10}),
+            ],
+                style={"display": 'flex'}),
             dcc.Textarea(
                 id='search_description',
                 value=None,
                 style={'width': '100%',
-                       'height': 40}),
+                       'height': 40,
+                       'margin-top': 10}),
             ],
             style={'width': '50%'}),
         html.Div([
-            html.Div("Note:"),
+            html.Div([
+                html.Div('Note:'),
+                daq.BooleanSwitch(
+                    id='bool_note',
+                    on=False,
+                    style={"margin-left": 10}),
+                ],
+                style={"display": 'flex'}),
             dcc.Textarea(
                 id='search_note',
                 value=None,
                 style={'width': '100%',
-                       'height': 40}),
+                       'height': 40,
+                       'margin-top': 10}),
             ],
             style={'width': '50%'})
         ],
@@ -210,6 +283,33 @@ def store_enabled_transaction(cell_search,
         data = None
 
     return data
+
+
+@app.callback(
+    [Output('search_amount_min', 'disabled'),
+     Output('search_amount_max', 'disabled'),
+     Output('search_type', 'disabled'),
+     Output('search_category', 'disabled'),
+     Output('search_sub_category', 'disabled'),
+     Output('search_occasion', 'disabled'),
+     Output('search_description', 'disabled'),
+     Output('search_note', 'disabled')],
+    [Input('bool_amount', 'on'),
+     Input('bool_category', 'on'),
+     Input('bool_type', 'on'),
+     Input('bool_occasion', 'on'),
+     Input('bool_description', 'on'),
+     Input('bool_note', 'on')])
+def store_enabled_transaction(bool_amount, bool_category, bool_type, bool_occasion, bool_description, bool_note):
+
+    list_enabled_components = [not bool_amount, not bool_amount,
+                               not bool_type,
+                               not bool_category, not bool_category,
+                               not bool_occasion,
+                               not bool_description,
+                               not bool_note]
+
+    return list_enabled_components
 
 
 def replace_object_id(df):
