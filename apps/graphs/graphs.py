91eb965f4b6efe61bee62f418cd0eb1763e2e106
Tom <fougeretom@gmail.com>
2022-04-03 19:49:12 +0200

    Add callback to the refresh button

--- apps/graphs/graphs.py
+++ apps/graphs/graphs.py
@@ -2,6 +2,8 @@ import datetime
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
+from dash import Input, Output, State, callback_context
+from app import app
 from apps.graphs.my_figures import \
     fig_indicators_revenue_expense_balance,\
     fig_expenses_vs_revenue, fig_expenses_vs_category, fig_expenses_vs_occasion, \
@@ -14,7 +16,7 @@ layout = html.Div(
         html.H1('Mes statistiques',
                 style={'textAlign': 'center'}),
         html.Div([
-            dbc.Button("Refresh", outline=True, color="secondary", className="me-1"),
+            dbc.Button("Refresh", outline=True, color="secondary", className="btn_refresh", id="btn_refresh"),
             dcc.Dropdown(id='dropdown_year_stat',
                          options=[
                              {'label': '2021', 'value': '2021'},
@@ -23,10 +25,12 @@ layout = html.Div(
                             ],
                          value='2021',
                          style={'width': '100%',
-                                'height': 40}
+                                'height': 40,
+                                'margin-left': 2}
                          ),
         ], style={'display': 'flex'}
         ),
+        html.Div(id='refresh_msg'),
         dcc.Graph(id='fig_indicators_revenue_expense_balance',
                   figure=fig_indicators_revenue_expense_balance(now.year),
                   style={'margin-top': 10}),
@@ -46,4 +50,11 @@ layout = html.Div(
                   figure=fig_cum_balance(now.year))
     ]
 )
+
+
+@app.callback(
+    Output('refresh_msg', 'children'),
+    Input('btn_refresh', 'n_clicks'))
+def refresh_page(n_click):
+    return str(n_click)
 
