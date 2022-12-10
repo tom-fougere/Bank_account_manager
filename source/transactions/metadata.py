169bcec0adf5749f9c8980aa244a254a623462df
Tom <fougeretom@gmail.com>
2022-12-10 17:15:06 +0100

    Re-organize metadataDB class (and UT)

--- source/transactions/metadata.py
+++ source/transactions/metadata.py
@@ -1,27 +1,33 @@
-from source.definitions import ALL_CATEGORIES, OCCASIONS, TYPE_TRANSACTIONS
+from source.categories import ALL_CATEGORIES, OCCASIONS, TYPE_TRANSACTIONS
 from utils.time_operations import str_to_datetime
 from source.db_connection.db_access import MongoDBConnection
+
+LIST_ATTRIBUTES_METADATA = [
+    'balance_in_bank',
+    'balance_in_db',
+    'balance_bias',
+    'categories',
+    'occasions',
+    'nb_transactions_db',
+    'types_transaction',
+    'date_balance_in_bank',
+    'date_last_import',
+]
 
 
 class MetadataDB:
     def __init__(self, name_connection, account_id):
         self.connection = MongoDBConnection(name_connection)
         self.account_id = account_id
-        self.balance_in_bank = None
-        self.balance_in_db = None
-        self.balance_bias = None
-        self.categories = None
-        self.occasions = None
-        self.types_transaction = None
-        self.nb_transactions_db = 0
-        self.date_balance_in_bank = {'dt': None,
-                                     'str': None}
-        self.date_last_import = {'dt': None,
-                                 'str': None}
 
-    def init_db(self, balance_in_bank, balance_in_db, balance_bias,
-                date_balance_in_bank, date_last_import, nb_trans_db=0,
-                categories=ALL_CATEGORIES, occasions=OCCASIONS, types=TYPE_TRANSACTIONS):
+        # Set default value
+        for attribute in LIST_ATTRIBUTES_METADATA:
+            setattr(self, attribute, None)
+
+    def set(self, balance_in_bank, balance_in_db, balance_bias,
+            date_balance_in_bank, date_last_import, nb_trans_db=0,
+            categories=ALL_CATEGORIES, occasions=OCCASIONS, types=TYPE_TRANSACTIONS):
+
         self.balance_in_bank = balance_in_bank
         self.balance_in_db = balance_in_db
         self.balance_bias = balance_bias
@@ -29,35 +35,41 @@ class MetadataDB:
         self.occasions = occasions
         self.types_transaction = types
         self.nb_transactions_db = nb_trans_db
+        self.date_balance_in_bank = dict()
         self.date_balance_in_bank['dt'] = date_balance_in_bank
         self.date_balance_in_bank['str'] = date_balance_in_bank.strftime("%d/%m/%Y")
+        self.date_last_import = dict()
         self.date_last_import['dt'] = date_last_import
         self.date_last_import['str'] = date_last_import.strftime("%d/%m/%Y")
 
-        data_to_ingest = {'account_id': self.account_id,
-                          'balance_in_bank': self.balance_in_bank,
-                          'balance_in_db': self.balance_in_db,
-                          'balance_bias': self.balance_bias,
-                          'categories': self.categories,
-                          'occasions': self.occasions,
-                          'nb_transactions_db': self.nb_transactions_db,
-                          'types_transaction': self.types_transaction,
-                          'date_balance_in_bank': self.date_balance_in_bank,
-                          'date_last_import': self.date_last_import}
+        # self.connection.collection.insert_one(data_to_ingest)
 
-        self.connection.collection.insert_one(data_to_ingest)
+    def set_from_db(self):
+        # Extract all data
+        data = self.connection.collection.find_one({'account_id': self.account_id})
 
-    def write_values_in_db(self):
+        if data is not None:
+            # remove useless field
+            del data['_id']
+            del data['account_id']
 
-        data_to_ingest = {'balance_in_bank': self.balance_in_bank,
-                          'balance_in_db': self.balance_in_db,
-                          'balance_bias': self.balance_bias,
-                          'categories': self.categories,
-                          'occasions': self.occasions,
-                          'nb_transactions_db': self.nb_transactions_db,
-                          'types_transaction': self.types_transaction,
-                          'date_balance_in_bank': self.date_balance_in_bank,
-                          'date_last_import': self.date_last_import}
+            # Set dict as class attributes
+            for key, value in data.items():
+                setattr(self, key, value)
+
+    def update_db(self):
+
+        data_to_ingest = {
+            'balance_in_bank': self.balance_in_bank,
+            'balance_in_db': self.balance_in_db,
+            'balance_bias': self.balance_bias,
+            'categories': self.categories,
+            'occasions': self.occasions,
+            'nb_transactions_db': self.nb_transactions_db,
+            'types_transaction': self.types_transaction,
+            'date_balance_in_bank': self.date_balance_in_bank,
+            'date_last_import': self.date_last_import,
+        }
 
         self.connection.collection.update(
             {
@@ -66,26 +78,8 @@ class MetadataDB:
             {
                 "$set": data_to_ingest
             },
-            upsert=False
+            upsert=True
         )
-
-    def get_all_values(self):
-
-        self.balance_in_bank = self.get_balance_in_bank()
-        self.balance_in_db = self.get_balance_in_db()
-        self.balance_bias = self.get_balance_bias()
-        self.categories = self.get_categories()
-        self.occasions = self.get_list_occasions()
-        self.types_transaction = self.get_types_transaction()
-        self.nb_transactions_db = self.get_nb_transactions_db()
-
-        date_balance_in_bank = self.get_date_balance_in_bank()
-        self.date_balance_in_bank = {'dt': str_to_datetime(date_balance_in_bank, date_format="%d/%m/%Y"),
-                                     'str': date_balance_in_bank}
-
-        date_last_import = self.get_date_last_import()
-        self.date_last_import = {'dt': str_to_datetime(date_last_import, date_format="%d/%m/%Y"),
-                                 'str': date_last_import}
 
     def update_values(self, new_values):
         for key, value in new_values.items():
@@ -98,76 +92,25 @@ class MetadataDB:
             elif key in self.__dict__.keys():
                 self.__setattr__(key, value)
 
-    def get_balance_in_bank(self):
-        result = self.connection.collection.find_one({'account_id': self.account_id}, ['balance_in_bank'])
-        return result['balance_in_bank']
+    def get_list_categories(self):
+        return list(self.categories.keys())
 
-    def get_balance_in_db(self):
-        result = self.connection.collection.find_one({'account_id': self.account_id}, ['balance_in_db'])
-        return result['balance_in_db']
-
-    def get_balance_bias(self):
-        result = self.connection.collection.find_one({'account_id': self.account_id}, ['balance_bias'])
-        return result['balance_bias']
-
-    def get_categories(self):
-        result = self.connection.collection.find_one({'account_id': self.account_id}, ['categories'])
-        return list(result['categories'].keys())
-
-    def get_category_info(self, category):
-        result = self.connection.collection.find_one({'account_id': self.account_id}, ['categories'])
-        return result['categories'][category]
-
-    def get_categories_and_sub(self):
-        result = self.connection.collection.find_one({'account_id': self.account_id}, ['categories'])
+    def get_list_categories_and_sub(self):
 
         cat_and_sub_cat = dict()
-        for cat, value in result['categories'].items():
+        for cat, value in self.categories.items():
             cat_and_sub_cat[cat] = list(value['Sub-categories'].keys())
 
         return cat_and_sub_cat
 
-    def get_sub_categories(self, category):
-        result = self.connection.collection.find_one({'account_id': self.account_id}, ['categories'])
-        return result['categories'][category]['Sub-categories']
-
-    def get_list_occasions(self):
-        result = self.connection.collection.find_one({'account_id': self.account_id}, ['occasions'])
-        return result['occasions']
-
     def get_default_occasion(self, category, sub_category=None):
-        result = self.connection.collection.find_one({'account_id': self.account_id}, ['categories'])
 
-        category_info = result['categories'][category]
+        category_info = self.categories[category]
         default_occasion = category_info['Default_occasion']
         if sub_category is not None and sub_category in list(category_info['Sub-categories'].keys()):
             default_occasion = category_info['Sub-categories'][sub_category]['Default_occasion']
 
         return default_occasion
-
-    def get_types_transaction(self):
-        result = self.connection.collection.find_one({'account_id': self.account_id}, ['types_transaction'])
-        return result['types_transaction']
-
-    def get_date_last_import(self):
-        result = self.connection.collection.find_one({'account_id': self.account_id}, ['date_last_import.str'])
-        return result['date_last_import']['str']
-
-    def get_date_balance_in_bank(self):
-        result = self.connection.collection.find_one({'account_id': self.account_id}, ['date_balance_in_bank.str'])
-        return result['date_balance_in_bank']['str']
-
-    def get_nb_transactions_db(self):
-        result = self.connection.collection.find_one({'account_id': self.account_id}, ['nb_transactions_db'])
-        return result['nb_transactions_db']
-
-    def update_balance_in_bank(self, balance):
-        self.connection.collection.update({'account_id': self.account_id},
-                                          {"$set": {'balance_in_bank': balance}}, upsert=False)
-
-    def update_balance_in_db(self, balance):
-        self.connection.collection.update({'account_id': self.account_id},
-                                          {"$set": {'balance_in_db': balance}}, upsert=False)
 
     def update_date_balance_in_bank(self, date):
 
@@ -186,13 +129,3 @@ class MetadataDB:
         self.connection.collection.update({'account_id': self.account_id},
                                           {"$set": {'date_last_import.str': date_str,
                                                     'date_last_import.dt': date_dt}}, upsert=False)
-
-    def update_nb_transactions(self, new_nb_trans_bank=0, new_nb_trans_db=0):
-
-        current_bn_trans_db = self.get_nb_transactions_db()
-
-        self.connection.collection.update({'account_id': self.account_id},
-                                          {"$set": {
-                                              'nb_transactions_db': current_bn_trans_db + new_nb_trans_db
-                                          }},
-                                          upsert=False)
